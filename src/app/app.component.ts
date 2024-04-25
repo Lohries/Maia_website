5c7d7d414aca7ac1086a7f0f0fb341b0b067f831
Lohries <lohries@Lohriess-MacBook-Air.local>
2024-04-25 15:10:37 -0300

    main components and styles adjusting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardsComponent } from './components/cards/cards.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, NavbarComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
